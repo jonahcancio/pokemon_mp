979321045a735922b27cb7cc0b9235eb3c85066d
jonahcancio <jonahcancio@gmail.com>
2018-04-19 16:38:13 +0800

    name change added

--- mp1.asm
+++ mp1.asm
@@ -17,7 +17,7 @@ decodeTionary:	.ascii	"^***************"	#0
 	.ascii	"***/,*0123456789"	#f
 stringBuffer:	.space	128
 bytesBuffer:	.space	128
-ramName:	.asciiz	""
+ramName:	.asciiz	"Pokemon Red (U) [S][BF].sav"
 ramBuffer:	.space	32768
 
 
@@ -56,19 +56,50 @@ ramBuffer:	.space	32768
 	lw	%reg, 0($sp)
 	add	$sp, $sp, 4
 .end_macro
-
-
-main:	li	$t0, 0x8a8b8c8d
-	sw	$t0, bytesBuffer
-	li	$t0, 0x50
-	sb	$t0, bytesBuffer+4
-	jal	PrintBytes
-	jal	DecodeBytes
+.macro	open_file(%name, %desc, %flag)
+	la	$a0, %name
+	li	$a1, %flag
+	li	$a2, 0
+	li	$v0, 13
+	syscall
+	move	%desc, $v0
+.end_macro
+.macro	read_file(%buffer, %desc, %max)
+	move	$a0, %desc
+	la	$a1, %buffer
+	li	$a2, %max
+	li	$v0, 14
+	syscall
+.end_macro
+.macro	write_file(%buffer, %desc, %max)
+	move	$a0, %desc
+	la	$a1, %buffer
+	li	$a2, %max
+	li	$v0, 15
+	syscall
+.end_macro
+.macro	close_file(%desc)
+	move	$a0, %desc
+	li	$v0, 16
+	syscall
+.end_macro
+main:	#READ FILE
+	open_file(ramName, $s6, 0)
+	read_file(ramBuffer, $s6, 32768)	
+	jal	GetName
 	print_string(stringBuffer)
+	print_char('\n')
+	close_file($s6)
+	
+	print_char('\n')
+	#WRITE FILE
+	open_file(ramName, $s6, 1)
 	scan_string(stringBuffer)
-	jal	StripNewLine
-	jal	EncodeString
-	jal	PrintBytes
+	jal	SetName
+	jal	FixCheckSum
+	write_file(ramBuffer, $s6, 32768)
+	close_file($s6)
+	
 	li	$v0, 10
 	syscall
 	
@@ -134,4 +165,56 @@ EncodeTionaryBreak:	sb	$t2, bytesBuffer($t0)
 EncodeBreak:	li	$t2, 0x50
 	sb	$t2, bytesBuffer($t0)	
 	pop($ra)
+	jr	$ra
+	
+#computes checksum and assigns it to address 0x3523
+FixCheckSum:	push($ra)
+	li	$t0, 0x2598
+	li	$t2, 0
+CheckSumLoop:	bge	$t0, 0x3523, CheckSumBreak
+	lbu	$t1, ramBuffer($t0)
+	add	$t2, $t2, $t1
+	andi	$t2, $t2, 0x000000FF
+	addi	$t0, $t0, 1
+	j	CheckSumLoop
+CheckSumBreak:	nor	$t2, $t2, $t2
+	andi	$t2, $t2, 0x000000FF
+	lbu	$t1, ramBuffer+0x3523
+	print_int($t1)
+	print_char('=')
+	print_char('>')
+	print_int($t2)
+	print_char('\n')
+	sb	$t2, ramBuffer+0x3523
+	pop($ra)
+	jr	$ra
+	
+#reads name from ramBuffer into bytesBuffer, decodes into stringBuffer, 
+GetName:	push($ra)
+	li	$t0, 0
+GetNameLoop:	bge	$t0, 7, GetNameElse
+	lbu	$t1, ramBuffer+0x2598($t0)
+	sb	$t1, bytesBuffer($t0)
+	beq	$t1, 0x50, GetNameBreak
+	addi	$t0, $t0, 1
+	j	GetNameLoop
+GetNameElse:	li	$t1, 0x50
+	sb	$t1, bytesBuffer($t0)
+GetNameBreak:	jal	DecodeBytes
+	pop($ra)
+	jr	$ra
+	
+#encodes name in stringBuffer into bytesBuffer and writes it into ramBuffer
+SetName:	push($ra)
+	jal	EncodeString
+	li	$t0, 0
+SetNameLoop:	bge	$t0, 7, SetNameElse
+	lbu	$t1, bytesBuffer($t0)
+	sb	$t1, ramBuffer+0x2598($t0)
+	beq	$t1, 0x50, SetNameBreak
+	addi	$t0, $t0, 1
+	j	SetNameLoop
+SetNameElse:	li	$t1, 0x50
+	sb	$t1, ramBuffer+0x2598($t0)
+SetNameBreak:	pop($ra)
 	jr	$ra
